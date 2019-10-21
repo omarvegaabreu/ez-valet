8e1fbb28ac6ec6d9ba4ab94911a65ea758ac1f63
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-10-20 20:52:43 -0400

    complete app

---
+++ client/public/sendEmail.js
@@ -0,0 +1,32 @@
+const nodemailer = require("nodemailer");
+const crypt = require("../../models/crypt");
+const emailDB = require("../../models/Users");
+const encryptedPassword = "xxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+var emailList = [];
+
+function pullEmails() {
+  emailList.push(emailDB.findmany({ users: "email" }));
+}
+
+const transport = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: "belljulio66@gmail.com",
+    pass: crypt.decrypt(encryptedPassword)
+  }
+});
+module.exports = function sendEmail() {
+  pullEmails();
+  const mailOptions = {
+    from: "belljulio66@gmail.com",
+    to: emailList,
+    subject: "SC Kitchen has a new listing!",
+    html: "message"
+  };
+  transport.sendMail(mailOptions, error => {
+    if (error) {
+      console.log(error);
+    }
+    // console.log(`Message sent: ${info.response}`);
+  });
+};
