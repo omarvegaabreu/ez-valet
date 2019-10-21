bb412c47c73cfaec920bdddb01b56731295935f7
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-10-20 22:00:43 -0400

    update json for heroku

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 //app.use(pino);
-//just to add
+
 app.post("/sms", (req, res) => {
   const twiml = new MessagingResponse();
 
